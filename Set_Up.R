a03e29e5fdd6263e8dff9958642f334dbc9c2111
JTGrimley <joseph.grimley@colostate.edu>
2024-10-07 09:29:50 -0600

    changed set up file

---
+++ Set_Up.R
@@ -0,0 +1,26 @@
+# a script for installing (if needed) and loading packages for this project.
+
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+# create a string of package names
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'rmarkdown',
+              'ggthemes',
+              'RColorBrewer',
+              'viridis',
+              'ggdark',
+              'plotly',
+              'lterdatasampler',
+              'rstatix')
+
+# use the packageLoad function we created on those packages
+packageLoad(packages)
